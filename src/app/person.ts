99d1b81bbd345bc7f887d04214fde32db7475087
Jhonatan Wesley Camêlo <jwcmade@gmail.com>
2023-05-29 15:33:58 -0300

    v1

---
+++ src/app/person.ts
@@ -0,0 +1,16 @@
+export interface Person {
+  id?: string;
+  name?: string;
+  born?: any;
+  gender?: string;
+  height?: number;
+  weight?: number;
+  notes?: string;
+  zipCode?: string;
+  street?: string;
+  number?: number;
+  complement?: string;
+  neighborhood?: string;
+  city?: string;
+  state?: string;
+}
